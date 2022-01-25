7c50c02f565c6bebf374353fa4339a2d2876c32d
abrunello <brunello@bludata.com>
2022-01-25 16:25:24 +0100

    adding layout for route switch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import Layout from "./components/layout/Layout";
-import MainNavigation from './components/layout/MainNavigation';
+import { Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
+import AllQuotes from './pages/AllQuotes';
+import QuoteDetail from './pages/QuoteDetail';
+import NewQuote from './pages/NewQuote';
 
 function App() {
   return (
     <>
-      <MainNavigation />
-      <Layout />
+      <Layout>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/quotes" />
+          </Route>
+          <Route path="/quotes" exact>
+            <AllQuotes />
+          </Route>
+          <Route path="/quotes/:quoteId">
+            <QuoteDetail />
+          </Route>
+          <Route path="/new-quote">
+            <NewQuote />
+          </Route>
+        </Switch>
+      </Layout>
     </>
   );
 }
--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,28 +1,14 @@
 import classes from "./Layout.module.css";
-import { Redirect, Switch } from "react-router-dom";
-import { Route } from "react-router-dom";
-import AllQuotes from "../../pages/AllQuotes";
-import QuoteDetail from "../../pages/QuoteDetail";
-import NewQuote from "../../pages/NewQuote";
+import MainNavigation from './MainNavigation';
 
 const Layout = (props) => {
   return (
-    <div className={classes.main}>
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/quotes" />
-        </Route>
-        <Route path="/quotes" exact>
-          <AllQuotes />
-        </Route>
-        <Route path="/quotes/:quoteId">
-          <QuoteDetail />
-        </Route>
-        <Route path="/new-quote">
-          <NewQuote />
-        </Route>
-      </Switch>
-    </div>
+    <>
+      <MainNavigation />
+      <main className={classes.main}>
+        {props.children}
+      </main>
+    </>
   );
 };
 
