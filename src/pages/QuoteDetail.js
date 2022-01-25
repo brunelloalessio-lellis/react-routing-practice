94ade3340e557a99e17b7bedd55a2d1e5880d7c2
abrunello <brunello@bludata.com>
2022-01-25 18:37:37 +0100

    adding http new quote
    http get list quote
    
    showing quote detail with request

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,25 +1,49 @@
 import { Link, Route, useParams, useRouteMatch } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
-
-const DUMMY_QUOTES = [
-  { id: "q1", author: "Max", text: "quote 1" },
-  { id: "q2", author: "Ale", text: "quote 2" },
-];
+import useHttp from '../hooks/use-http';
+import { getSingleQuote } from "../lib/api";
+import { useEffect } from 'react';
+import LoadingSpinner from "../components/UI/LoadingSpinner";
 
 const QuoteDetail = () => {
   const params = useParams();
-  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
   const match = useRouteMatch()
+  const {
+    quoteId
+  } = params
 
-  if (!quote) {
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error
+  }=useHttp(getSingleQuote, true)
+
+  useEffect(()=>{
+    sendRequest(quoteId)
+  },[sendRequest,quoteId])
+
+  if(status === 'pending'){
+    return <div className="centered">
+      <LoadingSpinner/>
+    </div>
+  }
+
+  if(error){
+    return <div className="centered">
+      {error}
+    </div>
+  }
+
+  if (!loadedQuote.text) {
     return <p>No quote found</p>;
   }
 
   return (
     <>
       <h1>QuoteDetail page</h1>
-      <HighlightedQuote text={quote.text} author={quote.author} />
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
       <Route path={match.path} exact>
         <div className="centered">
           <Link className="btn--flat" to={`${match.path}/comments`}>
